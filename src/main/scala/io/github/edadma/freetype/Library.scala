0ace0620efede9107e73236e886f77e01e8eeab1
Edward Adrian Maxedon <edadma@gmail.com>
2023-03-01 17:00:04 -0500

    Library

--- src/main/scala/io/github/edadma/freetype/Library.scala
+++ src/main/scala/io/github/edadma/freetype/Library.scala
@@ -2,17 +2,18 @@ package io.github.edadma.freetype
 
 import scala.scalanative.unsafe._
 
-import io.github.edadma.freetype.extern.LibFreeType as lib
+import io.github.edadma.freetype.extern.LibFreeType.*
 
-implicit class Library(val library: lib.FT_Library) extends AnyVal:
-  def doneFreeType: Int = lib.FT_Done_FreeType(library)
-  def FT_New_Face(filepathname: String, face_index: Long): Either[Int, Face] =
-    val aface = stackalloc[lib.FT_Face]()
+implicit class Library(val libraryptr: FT_Library) extends AnyVal:
+  def doneFreeType: Int = FT_Done_FreeType(libraryptr)
+  def newFace(filepathname: String, face_index: Long): Either[Int, Face] =
+    val aface = stackalloc[FT_Face]()
 
-    Zone(implicit z => lib.FT_New_Face(library, toCString(filepathname), face_index, aface)) match
+    Zone(implicit z => FT_New_Face(libraryptr, toCString(filepathname), face_index, aface)) match
       case 0   => Right(!aface)
       case err => Left(err)
 
-implicit class Face(val library: lib.FT_Face) extends AnyVal
+implicit class Face(val faceptr: FT_Face) extends AnyVal:
+  def DoneFace: Int = FT_Done_Face(faceptr)
 
-def errorString(error_code: Int): String = fromCString(lib.FT_Error_String(error_code))
+def errorString(error_code: Int): String = fromCString(FT_Error_String(error_code))
