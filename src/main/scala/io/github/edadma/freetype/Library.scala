a9cf0a698382ff9b76ce1bbd2619a15697b035e1
Edward Adrian Maxedon <edadma@gmail.com>
2023-03-01 18:15:09 -0500

    Library

--- src/main/scala/io/github/edadma/freetype/Library.scala
+++ src/main/scala/io/github/edadma/freetype/Library.scala
@@ -24,6 +24,13 @@ implicit class Face(val faceptr: FT_Face) extends AnyVal:
   def loadChar(char_code: Long, load_flags: Int): Int = FT_Load_Char(faceptr, char_code.toULong, load_flags)
   def renderGlyph(render_mode: Int): FT_Error =
     FT_Render_Glyph(!(faceptr + FACE_GLYPH).asInstanceOf[Ptr[FT_GlyphSlot]], render_mode)
-  def bitmap(idx: Int): Int =
+  def bitmap(idx: Int): Bitmap =
+    (!(faceptr + FACE_GLYPH).asInstanceOf[Ptr[FT_GlyphSlot]] + FACE_GLYPH_BITMAP).asInstanceOf[Ptr[FT_Bitmap]]
+
+implicit class Bitmap(val bitmapptr: Ptr[FT_Bitmap]) extends AnyVal:
+  def rows: Int = bitmapptr._1.toInt
+  def width: Int = bitmapptr._2.toInt
+  def pitch: Int = bitmapptr._3
+  def buffer(idx: Int): Int = (!(bitmapptr._4 + idx)).toInt & 0xff
 
 def errorString(error_code: Int): String = fromCString(FT_Error_String(error_code))
