e301992244eac83754ce02bff26d7bdb4cf61852
Edward A Maxedon <edadma@gmail.com>
2024-09-12 21:17:53 -0400

    correct typing errors

--- src/main/scala/io/github/edadma/freetype/Library.scala
+++ src/main/scala/io/github/edadma/freetype/Library.scala
@@ -18,7 +18,7 @@ implicit class Library(val libraryptr: FT_Library) extends AnyVal:
   def newFace(filepathname: String, face_index: Long): Either[Int, Face] =
     val aface = stackalloc[FT_Face]()
 
-    Zone { FT_New_Face(libraryptr, toCString(filepathname), face_index.asInstanceOf[FT_Long], aface) } match
+    Zone { FT_New_Face(libraryptr, toCString(filepathname), face_index.toSize.asInstanceOf[FT_Long], aface) } match
       case 0   => Right(!aface)
       case err => Left(err)
 
@@ -33,7 +33,7 @@ implicit class Face(val faceptr: FT_Face) extends AnyVal:
   def setPixelSizes(pixel_width: Int, pixel_height: Int): Int =
     FT_Set_Pixel_Sizes(faceptr, pixel_width.toUInt, pixel_height.toUInt)
   def loadChar(char_code: Long, load_flags: Int): Int =
-    FT_Load_Char(faceptr, char_code.toULong.asInstanceOf[FT_ULong], load_flags)
+    FT_Load_Char(faceptr, char_code.toUSize.asInstanceOf[FT_ULong], load_flags)
   def renderGlyph(render_mode: RenderMode): FT_Error =
     FT_Render_Glyph(
       !(faceptr.asInstanceOf[Ptr[Byte]] + FACE_GLYPH).asInstanceOf[Ptr[FT_GlyphSlot]],
